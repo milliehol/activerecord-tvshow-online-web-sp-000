acf01b755bed3c75fdd16eeafe7077c8774a7b36
Carla Holcomb <carla.programmer@gmail.com>
2020-01-12 19:02:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,24 @@
 class Show < ActiveRecord::Base
   
+  
+def Show
+  show = Show.new
+  show.title = "This is a title."
+  show.save
+end
+
+def can_be_created_with_a_hash_of_attributes
+  # Initialize movie and then and save it
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
+  movie = Movie.create(attributes)
+end
+  
     def Show::highest_rating
     Show.maximum("rating")
   end
